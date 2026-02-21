Finishing my-model-baisic code; overflow problems fixed

mnist-test/test-my-model.py
@@ -8,18 +8,21 @@ data = np.load("mnist-test\\mnist.npz")
 x_train = data['x_train']
 y_train = data['y_train']
 
-print("Datapoints: ", len(x_train))
-print("Shape: ", x_train[0].shape)
+
 
 
 # Transforming to vectors
 print("Transforming matrixs to vectors of shape: ", x_train[0].shape[0]*x_train[0].shape[1])
-x_train = [x_train[i].reshape(-1) for i in range(len(x_train))]
+x_train = [x_train[i].reshape(-1)*1/255 for i in range(len(x_train))] # Normalize data to avoid overflow problems
 
-model = NeuralNetwork([784,125,125,10],activations=["ReLU","ReLU","Softmax"])
+model = NeuralNetwork([784,524,10],activations=["ReLU","ReLU","ReLU"])
 
 epochs = 100
 data = list(zip(x_train,y_train))
+print("Datapoints: ", len(data))
+print("Shape: ", data[0][0].shape)
+data, _ = split(data, 0.1)
+
 loss_ep, t_ep = model.train(data,epochs,0.01,loss=MSE())
 
 fig, ax1 = plt.subplots()